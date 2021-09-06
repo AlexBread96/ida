dcd0359ecdd4b05d79989199852cd4f6584705c8
Alexandr Bukhanov <bukhanov96@mail.ru>
2021-09-06 20:48:33 +0300

    Final create

--- app/js/main.js
+++ app/js/main.js
@@ -1,52 +1,49 @@
-const form = document.querySelector("form");
-const addInput = document.querySelectorAll(".add__input");
-const container = document.querySelector(".add__cards");
+const form = document.querySelector("form"),
+  addInput = document.querySelectorAll(".add__input"),
+  container = document.querySelector(".add__cards"),
+  required = document.querySelectorAll(".required"),
+  addSubmit = document.querySelector(".add__submit");
 
 let items = [];
 
 const getInput = () => {
-  event.preventDefault();
-  let arr = [];
   addInput.forEach((element) => {
-    arr.push(element.value);
+    items.push(element.value);
   });
-  items.push({
-    name: arr[0],
-    dsc: arr[1],
-    url: arr[2],
-    price: arr[3],
-  });
-  //   console.log(arr);
 };
 const renderItems = () => {
   container.insertAdjacentHTML(
     "beforeend",
     `
       <div class="add__card">
-      <button data-index="${items.length - 1}" class="add__remove">
+      <button class="add__remove">
    
       </button>
       <div class="add__img">
         <img
-          src="${items[items.length - 1].url}"
+          src="${items[2]}"
           alt=""
           srcset=""
         />
       </div>
       <div class="add__text">
-        <div class="add__name">${items[items.length - 1].name}</div>
-        <div class="add__dsc">${items[items.length - 1].dsc}</div>
-        <div class="add__price">${items[items.length - 1].price} руб</div>
+        <div class="add__name">${items[0]}</div>
+        <div class="add__dsc">${items[1]}</div>
+        <div class="add__price">${items[3]} руб</div>
       </div>
     </div>
       `
   );
+  items = [];
   const removeBtn = document.querySelectorAll(".add__remove");
   removeBtn.forEach((element) => {
     element.addEventListener("click", (e) => {
       console.log(e.target);
-      e.target.parentNode.remove();
-      items = [];
+      e.target.parentNode.style.transform = "scale(0)";
+      e.target.parentNode.style.opacity = "0";
+      setTimeout(() => {
+        e.target.parentNode.remove();
+      }, 300);
     });
   });
 };
@@ -57,11 +54,51 @@ const clearForm = () => {
 };
 form.addEventListener("submit", () => {
   event.preventDefault();
-  getInput();
-  console.log(items);
-  renderItems();
-  clearForm();
+  // required.forEach((element) => {
+  //   element.classList.remove("active");
+  // });
+  if (
+    required[0].lastElementChild.value != "" &&
+    required[1].lastElementChild.value != "" &&
+    required[2].lastElementChild.value != ""
+  ) {
+    getInput();
+    renderItems();
+    clearForm();
+    addSubmit.removeAttribute("disabled");
+  } else {
+    addSubmit.setAttribute("disabled", "disabled");
+    required.forEach((element) => {
+      if (element.lastElementChild.value != "") {
+        element.classList.remove("active");
+      } else {
+        element.classList.add("active");
+      }
+    });
+  }
+});
+form.addEventListener("input", () => {
+  required.forEach((element) => {
+    element.classList.remove("active");
+  });
+  if (
+    required[0].lastElementChild.value != "" &&
+    required[1].lastElementChild.value != "" &&
+    required[2].lastElementChild.value != ""
+  ) {
+    addSubmit.removeAttribute("disabled");
+  } else {
+    addSubmit.setAttribute("disabled", "disabled");
+    required.forEach((element) => {
+      if (element.lastElementChild.value != "") {
+        element.classList.remove("active");
+      } else {
+        element.classList.add("active");
+      }
+    });
+  }
 });
 
+const validation = () => {};
 // .remove();
 // элемент.parentNode
